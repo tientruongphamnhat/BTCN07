cd91ad82473ddc8c64ccb0616942807afe4e7976
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2019-10-21 22:10:59 +0700

    use React Router

---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, FormGroup, Label, Input } from 'react-bootstrap';
+
+function login() {
+  return (
+    <Form>
+      <h2>
+        <span className="font-weight-bold">Đăng Nhập</span>
+      </h2>
+    </Form>
+  );
+}
+
+export default login;
---
+++ src/components/Register/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, FormGroup, Label, Input } from 'react-bootstrap';
+
+function Register() {
+  return (
+    <Form>
+      <h2>
+        <span className="font-weight-bold">Đăng Ký</span>
+      </h2>
+    </Form>
+  );
+}
+
+export default Register;
